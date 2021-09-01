a1c3d20b46ace282af7a49643eaa5f97c7bc9f0a
Jed Crandall <jedimaestro@asu.edu>
2021-09-01 10:55:49 -0700

    fixed slides and build.sh

--- courses/cse365fall2021/build.sh
+++ courses/cse365fall2021/build.sh
@@ -2,5 +2,5 @@
 
 python3 -m markdown -x markdown.extensions.tables syllabus.md > syllabus.html
 python3 -m markdown -x markdown.extensions.tables _index.md > index.html
-python3 -m markdown -x markdown.extensions.tables hw1part1.md > hwpart1.html
+python3 -m markdown -x markdown.extensions.tables hw1part1.md > hw1part1.html
 
